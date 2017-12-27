6383621a093f5add97d4a3bdecf5f1900aa8200c
k43zhu <k43zhu@edu.uwaterloo.ca>
2017-12-27 16:08:12 -0500

    finished the coin problem fucntion for python

--- nugget.py
+++ nugget.py
@@ -1,13 +1,21 @@
-def nuggets(a, n):
+def gcd(a, b):
+    a, b = max(a, b), min(a, b)
+    if (b == 0):
+        return a
+    return gcd(b, a % b)
+        
+def nuggets(a):
+    n = len(a)
     divisor = a[0]
-    mod = INF
+    mod = min(a)
+
     for i in range(n):
         divisor = gcd(divisor, a[i])
-        if (a[i] < mod):
-            mod = a[i]
     
     if (divisor != 1):
         return -1
+    elif (mod == 1):
+        return 0
     
     mod_nums = [0 for x in range(mod)]
     
@@ -16,29 +24,39 @@ def nuggets(a, n):
     for i in range(n):
         current  = a[i] % mod
         multiple = int(a[i]/mod)
-        if (mod_nums[current] = 0):
-            mod_nums[current] = muliple
+        if (mod_nums[current] == 0):
+            mod_nums[current] = multiple
         else:
             mod_nums[current] = min(mod_nums[current], multiple)
     
-    temp = max(mod_nums)
-    for i in range(temp):
+    max_mod = max(mod_nums)
+    for i in range(max_mod + 1):
         mod_table.append([])
     
     for i in range(mod):
-        mod_table[mon_nums[i]].append(i)
-
-    count = 2
-    while (not mod_table[0]):
-        for i in range(1, int(count/2)):
-            
-
-
+        mod_table[mod_nums[i]].append(i)
     
+    mod_table[1].remove(0)
 
-def gcd(a, b):
-    a, b = max(a, b), min(a, b)
-    if (b == 0):
-        return a
-    return gcd(b, a % b)
-        
+    print(mod_table[1])
+    count = 2
+    while (mod_table[0]):
+        for i in range(1, int(count/2) + 1):
+            for j in range(len(mod_table[i])):
+                for k in range(len(mod_table[count-i])):
+                    new_nugget = mod_table[i][j] + mod_table[count-i][k]
+                    over_mod = int(new_nugget/mod)
+                    new_nugget = new_nugget % mod
+                    if (new_nugget in mod_table[0]):
+                        while (max_mod < count + over_mod):
+                            mod_table.append([])
+                            max_mod += 1
+                        mod_table[count + over_mod].append(new_nugget)
+                        mod_table[0].remove(new_nugget)
+        count += 1
+
+    final = max(mod_table[max_mod]) + max_mod*mod - mod
+    return final    
+
+a = [11, 13, 17]
+print(nuggets(a))
